1b3162ced7aa782c4780b44b3d5c84d63f4f8b0c
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2022-07-23 11:07:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['601169655956',''] //owner number +27634090203
+global.wame = ['601169655956'] 
+global.pemilik = ['601169655956'] //owner number +27634090203
+global.premium = ['601169655956'] //premium number +27634090203
+global.pengguna = 'Khais' //username
+global.footer = 'STAJ BOT INC'
+global.botnma = 'STAJ BOT INC' //bot name
+global.botname = 'STAJ BOT V'
+global.ownernma = 'Khais' //owner name
+global.packname = 'Paktam' //sticker package name
+global.author = 'By Paktam Meme' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
